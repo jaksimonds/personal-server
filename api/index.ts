f98ba4492654eb139c36645d45342aec718517a0
jaksimonds <jaksimonds@gmail.com>
2023-07-13 18:03:09 -0500

    Server configuration. Updating the src directory to the api directory per the Vercel serverless functions docs.

---
+++ api/index.ts
@@ -0,0 +1,43 @@
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from '@apollo/server/express4'
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import express from 'express'
+import http from 'http'
+import cors from 'cors'
+import bodyParser from "body-parser";
+import { typeDefs, resolvers } from "./schema";
+
+interface ServerContext {
+  token?: String
+}
+
+const server = async () => {
+  const app = express()
+
+  const httpServer = http.createServer(app)
+
+  const server = new ApolloServer<ServerContext>({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({httpServer})],
+  })
+
+  await server.start()
+
+  app.use(
+    '/',
+    cors<cors.CorsRequest>(),
+    bodyParser.json({
+      limit: '50mb'
+    }),
+    expressMiddleware(server, {
+      context: async ({req}) => ({ token: req.headers.token })
+    })
+  )
+
+  await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve))
+
+  console.log(`Server ready`);
+}
+
+export default server
