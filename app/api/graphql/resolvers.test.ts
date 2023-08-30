763b9a40b5ed24994abf1eb9c57c15b030022559
jaksimonds <jaksimonds@gmail.com>
2023-08-29 20:54:25 -0500

    Jest configuration. Added Jest to the project and created test files for all files inside the app folder. Brought all tests to full coverage.

---
+++ app/api/graphql/resolvers.test.ts
@@ -0,0 +1,57 @@
+import resolvers from './resolvers'
+
+jest.mock('./data', () => ({
+  pages: [
+    {
+      slug: 'test'
+    }
+  ],
+  projects: [
+    {
+      id: 0,
+      year: 2020,
+      slug: 'test'
+    },
+    {
+      id: 1,
+      year: 2021,
+      slug: 'foo'
+    },
+    {
+      id: 2,
+      year: 2020,
+      slug: 'bar'
+    },
+    {
+      id: 3,
+      year: 2022,
+      slug: 'other'
+    }
+  ]
+}))
+
+describe('data', () => {
+  test('tests projects resolver', () => {
+    const projectsResponse = resolvers.Query.projects()
+
+    expect(projectsResponse).toBeDefined()
+  })
+
+  test('tests featuredProjects resolver', () => {
+    const featuredProjectsResponse = resolvers.Query.featuredProjects()
+
+    expect(featuredProjectsResponse).toBeDefined()
+  })
+
+  test('tests project resolver', () => {
+    const projectResponse = resolvers.Query.project(null, { slug: 'test' })
+
+    expect(projectResponse).toBeDefined()
+  })
+
+  test('tests page resolver', () => {
+    const pageResponse = resolvers.Query.page(null, { slug: 'test' })
+
+    expect(pageResponse).toBeDefined()
+  })
+})
---
+++ app/layout.test.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/display-name */
+import React from 'react'
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import RootLayout from './layout'
+
+describe('RootLayout', () => {
+  beforeAll(() => {
+    jest.spyOn(console, 'error').mockImplementation(() => null)
+  })
+
+  afterAll(() => {
+    jest.clearAllMocks()
+  })
+
+  test('tests default', () => {
+    const { container } = render(<RootLayout><section>test</section></RootLayout>)
+
+    const footer = container.querySelector('footer')
+    expect(footer).toBeInTheDocument()
+  })
+})
